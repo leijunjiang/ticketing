233ff0e60cadd4ebe35580e201156f8da07a7409
leijun <leijunjiang@gmail.com>
2024-06-22 16:37:35 +0200

    signout path done

---
+++ client/pages/auth/signout.js
@@ -0,0 +1,26 @@
+import { useEffect } from 'react';
+import Router from 'next/router';
+import useRequest from '../../hooks/use-request';
+
+const signout = () => {
+  const { doRequest } = useRequest({
+    url: '/apo/users/signout',
+    method: 'post',
+    body: {},
+    onSuccess: () => {
+      Router.push('/')
+    }
+  })
+
+  useEffect(() => {
+    doRequest();
+  });
+
+  return (
+    <div>
+      Signing you out ...
+    </div>
+  )
+}
+
+export default signout;
