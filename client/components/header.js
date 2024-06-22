b5d2819a4b84c6548f45346eb10c0b4cea509f70
leijun <leijunjiang@gmail.com>
2024-06-22 16:40:21 +0200

    buf fixed

--- client/components/header.js
+++ client/components/header.js
@@ -3,7 +3,7 @@ import Link from 'next/link';
 const Header = ({ currentUser }) => {
   const links = [
     !currentUser && { label: 'Sign Up', href: '/auth/signup' },
-    !currentUser && { label: 'Sign In', href: '/auth/signip' },
+    !currentUser && { label: 'Sign In', href: '/auth/signin' },
     currentUser && { label: 'Sign Out', href: '/auth/signout' },
   ]
     .filter(linkConfig => linkConfig)
